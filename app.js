4a48ca877769ab1e9069080395bdfe2f69656d59
vaishnavi-agrawall <101472652+vaishnavi-agrawall@users.noreply.github.com>
2024-03-30 12:32:26 +0530

    Add files via upload
    updated

--- app.js
+++ app.js
@@ -20,9 +20,18 @@ app.use(express.static("public"));
 
 
 app.get( '/', (req,res)=>{
+    res.render('home' );
+
+});
+
+
+app.get( '/login', (req,res)=>{
     res.render('login' );
 
 });
+
+
+
 
 app.get( '/signup', (req,res)=>{
     res.render('signup' );
