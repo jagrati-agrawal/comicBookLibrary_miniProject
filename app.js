4807fdb418b3c564c120b1f7730a217c1903f780
jagrati-agrawal <ka7466661@gmail.com>
2024-05-08 19:49:58 +0530

    commit

--- app.js
+++ app.js
@@ -1,58 +1,80 @@
-const express=require( 'express');
-const path=require('path');
-const bcrypt=require('bcrypt') ;
-const mongoose=require('mongoose');
+const express = require('express');
+const path = require('path');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
 const user = require('./models/user');
-const app=express();
+const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/user')
-  .then(() => { console.log('DB connected') })
-  .catch(() => { console.log('DB not connected') });
+    .then(() => { console.log('DB connected') })
+    .catch(() => { console.log('DB not connected') });
 
 
-app.set( "view engine", "ejs" );
-app.set('views',path.join(__dirname,'views'));
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static("public"));
 
-
-
-app.get( '/', (req,res)=>{
-    res.render('home' );
+app.get('/', (req, res) => {
+    res.render('login');
 
 });
 
 
-app.get( '/login', (req,res)=>{
-    res.render('login' );
+app.get('/signup', (req, res) => {
+    res.render('signup');
 
 });
 
-
-
-
-app.get( '/signup', (req,res)=>{
-    res.render('signup' );
-
-});
-app.get('/signup/home',(req,res)=>{
-    res.render('home');
+app.get('/signup/login', (req, res) => {
+    res.render('login');
 });
 
 
-app.post('/signup', async(req,res)=>{
-    const data= {
-        name: req.body.username ,
-        password:req.body.password  
+app.post('/signup', async (req, res) => {
+    const data = {
+        name: req.body.username,
+        password: req.body.password
     }
-    const userData= await user.insertMany(data);
-    console.log(userData);
-    user.save();
-    res.redirect("/home");
+    const existingUser = await user.findOne({ name: data.name });
+    if (existingUser) {
+        res.send("user is already exists.Please try another username");
+    }
+    else {
+        v
+        const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+        data.password = hashedPassword;
+
+        const userData = await user.insertMany(data);
+        console.log(userData);
+       
+    }
 
 });
+
+app.post("/login",async(req,res)=>{
+    try{
+        const check=await user.findOne({name:req.body.username});
+        if(!check){
+            res.send("user not found");
+        }
+
+        const isPasswordMatch=await bcrypt.compare(req.body.password,check.password);
+        if (!isPasswordMatch) {
+            res.render("home");
+    }
+    else{
+        req.send("wrong password");
+    }
+
+}
+catch(err){
+    console.error(err.message);
+}
+
+})
 
 
 
